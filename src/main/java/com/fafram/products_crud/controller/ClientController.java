a792a1ea5d933cc3aa8f12e40d870c28d2985fad
Thiago Monteiro <ttggttb40@gmail.com>
2024-11-07 21:34:29 -0300

    Atualização do controller com comentários e uma mensagem de retorno ao cadastrar o cliente

--- src/main/java/com/fafram/products_crud/controller/ClientController.java
+++ src/main/java/com/fafram/products_crud/controller/ClientController.java
@@ -17,20 +17,23 @@ public class ClientController {
     @Autowired
     private IClientService service;
 
+    // Exibe a página de cadastro de cliente
     @GetMapping("/register")
     public String showRegistration() {
         return "registerClientPage";
     }
 
+    // Salva os dados do cliente
     @PostMapping("/save")
     public String save(@ModelAttribute Client client,
                        Model model) {
         Client c = service.saveClient(client);
-        String message = "Client " + c.getClass() + " saved!";
+        String message = "Client " + c.getName() + " saved!";  // Atualizado para mostrar o nome do cliente
         model.addAttribute("message", message);
         return "registerClientPage";
     }
 
+    // Exibe a lista de todos os clientes
     @GetMapping("/getAllClients")
     public String getAllClients(Model model) {
         List<Client> clients = service.getAllClients();
@@ -38,27 +41,29 @@ public class ClientController {
         return "listClientsPage";
     }
 
+    // Exibe a página de edição do cliente
     @GetMapping("/edit")
     public String edit(Model model,
                        @RequestParam Long id,
                        RedirectAttributes redirectAttributes) {
-
         Client client = service.getClientById(id);
         model.addAttribute("client", client);
         return "editClientPage";
     }
 
+    // Atualiza as informações do cliente
     @PostMapping("/update")
     public String update(@ModelAttribute Client client,
                          RedirectAttributes redirectAttributes) {
         service.updateClient(client);
-        return "redirect:getAllClients";
+        return "redirect:/client/getAllClients";  // Corrigido para redirecionar corretamente
     }
 
+    // Deleta um cliente
     @GetMapping("/delete")
     public String delete(@RequestParam Long id,
                          RedirectAttributes redirectAttributes) {
         service.deleteClientById(id);
-        return "redirect:getAllClients";
+        return "redirect:/client/getAllClients";  // Corrigido para redirecionar corretamente
     }
 }
